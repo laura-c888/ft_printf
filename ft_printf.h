97a0651cbcfac785b0a84e0476374fe44af9d23c
lchehbi <chehbilaura@gmail.com>
2026-05-10 22:32:06 +0200

    ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -11,10 +11,10 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(const char *format, ...);
 
